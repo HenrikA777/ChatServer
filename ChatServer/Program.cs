04ec962d917496afea5468d73053179a3ee68fa8
Henrik Andersen <henr563a@edu.easj.dk>
2019-03-14 18:57:17 +0100

    q

---
+++ ChatServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    using System.Net.Sockets;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                TcpClient client = Server.AcceptClient().Result;
+                ChatRoom.HandShake(client);
+            }
+        }
+    }
+}
